6dcac49b50463ccc9da6e515eab6fcaf07b8ae24
Sander <sander@concept7.nl>
2024-12-05 23:10:35 +0100

    fixed app name & removed post seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         Post::factory()
-            ->count(25)
+            ->count(1)
             ->create();
 
         Notification::make()
