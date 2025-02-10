a7a88615b438150d752d28c92112b2d6fab96fbe
Sander <sander@concept7.nl>
2025-02-10 14:34:02 +0100

    footer spacing fix

--- resources/views/components/sections/footer.blade.php
+++ resources/views/components/sections/footer.blade.php
@@ -1,7 +1,7 @@
-<footer class="text-white w-full text-center px-2 md:px-4 my-12 md:my-16">
-    <h2 class="text-3xl md:text-6xl xl:text-7xl text-left max-w-5xl">For new business inquiries please email to <a href="mailto:veldwischthijs@gmail.com" class="text-neutral-400 underline hover:text-white trasistion ease-in-out duration-100">veldwischthijs@gmail.com</a></h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 mt-10 md:mt-20">
-        <div class="mr-6 text-left text-lg md:text-xl">
+<footer class="px-2 my-12 w-full text-center text-white md:px-4 md:my-16">
+    <h2 class="max-w-5xl text-3xl text-left md:text-6xl xl:text-7xl">For new business inquiries please email to <a href="mailto:veldwischthijs@gmail.com" class="underline duration-100 ease-in-out text-neutral-400 hover:text-white trasistion">veldwischthijs@gmail.com</a></h2>
+    <div class="grid grid-cols-1 mt-4 md:grid-cols-2 md:mt-6">
+        <div class="mr-6 text-lg text-left md:text-xl">
           <p class="text-neutral-400">All contents of this website are the property of Studio Thijs Veldwisch. No part of this site, including all text and images, may be reproduced in any form without the prior written consent of Studio Thijs Veldwisch. © 2024 STUDIO THIJS VELDWISCH. All rights reserved. 
           </p>
         </div>
@@ -18,10 +18,10 @@
                   month: 'long', day: 'numeric', year: 'numeric' 
               }).format(new Date()) 
           }, 1000)"
-          class="mr-6 text-left text-lg md:text-xl mt-4 md:mt-0"
+          class="mt-4 mr-6 text-lg text-left md:text-xl md:mt-0"
         >
           <p class="text-neutral-400" x-text="time"></p>
-          <p class="text-neutral-400">Development by <a href="https://sanderr.site/" class="text-white underline hover:text-neutral-300 trasistion ease-in-out duration-100">Sander</a></p>
+          <p class="text-neutral-400">Development by <a href="https://sanderr.site/" class="text-white underline duration-100 ease-in-out hover:text-neutral-300 trasistion">Sander</a></p>
         </div>
     </div>
 </footer>
